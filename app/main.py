774c72138b54ecda200da986745a4bf646b4b243
katab99 <bujdosookata@gmail.com>
2026-01-25 22:17:45 +0100

    Refactor logging configuration and add debug statements for prediction process.

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ from ml.house_price_regressor import HousePriceRegressor
 from .utils import convert_data_to_array
 from config import PRICE_PRED_MODEL
 
-# import logging
+import os
+import logging
 
 
 # models
@@ -23,18 +24,28 @@ class Prediction(BaseModel):
 
 app = FastAPI()
 
-# logger = logging.basicConfig()
+
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
+logger = logging.basicConfig(
+    level=LOG_LEVEL,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+logger = logging.getLogger(__name__)
 
 
 # routes
 @app.post("/predict")
 async def post_predict(features: Features):
+    logger.debug(f"Predicting with features: {features}")
 
     data = convert_data_to_array(features)
     model = HousePriceRegressor(model_path=PRICE_PRED_MODEL)
     price_pred = model.predict(data)
 
     response = Prediction(price=price_pred[0])
+
+    logger.debug(f"Prediction result: {response}")
 
     return response
 
