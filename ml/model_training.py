2fecb1b565b1359bae0532857e50c50cb060ac9c
katab99 <bujdosookata@gmail.com>
2026-01-27 18:00:55 +0100

    Refactor model training script for improved readability and consistency

--- ml/model_training.py
+++ ml/model_training.py
@@ -6,21 +6,23 @@ from config import REAL_ESTATE_DATA, PRICE_PRED_MODEL
 house_data = pd.read_csv(REAL_ESTATE_DATA)
 
 # featuring columns from `data_exploration.ipynb`
-features = ['X1 transaction date', 
-    'X2 house age', 
-    'X3 distance to the nearest MRT station', 
-    'X4 number of convenience stores', 
-    'X5 latitude', 
-    'X6 longitude']
+features = [
+    "X1 transaction date",
+    "X2 house age",
+    "X3 distance to the nearest MRT station",
+    "X4 number of convenience stores",
+    "X5 latitude",
+    "X6 longitude",
+]
 
 # create training and validation data
 X = house_data[features]
-y = house_data['Y house price of unit area']
-X_train, X_val, y_train, y_val = train_test_split(X, y, random_state = 0)
+y = house_data["Y house price of unit area"]
+X_train, X_val, y_train, y_val = train_test_split(X, y, random_state=0)
 
-#training model
+# training model
 model = HousePriceRegressor()
-model.fit(X_train, y_train)
+model.fit(X_train.values, y_train.values)
 
 # export trained model
 model.save_model(PRICE_PRED_MODEL)
